717ad9c9d16ac9cf079cfd9f806dca15264d551b
Abner Cordeiro de Almeida <abnersolk@gmail.com>
2026-09-10 11:21:05 -0300

    start

---
+++ src/main/java/com/example/Login_DIAW/LoginDiawApplication.java
@@ -0,0 +1,13 @@
+package com.example.Login_DIAW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginDiawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginDiawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Login_DIAW/LoginDiawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Login_DIAW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginDiawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
